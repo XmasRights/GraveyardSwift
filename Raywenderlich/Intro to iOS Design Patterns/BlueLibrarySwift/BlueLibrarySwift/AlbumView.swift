09826a9087ecfc68b1a0df0b3ecd4f0164861256
Christopher Fonseka <chris.fonseka@googlemail.com>
2015-10-11 23:46:01 +0100

    Added an AlbumView class (UIView)

---
+++ Raywenderlich/Intro to iOS Design Patterns/BlueLibrarySwift/BlueLibrarySwift/AlbumView.swift
@@ -0,0 +1,62 @@
+//
+//  AlbumView.swift
+//  BlueLibrarySwift
+//
+//  Created by Christopher Fonseka on 11/10/2015.
+//  Copyright © 2015 Raywenderlich. All rights reserved.
+//
+
+import UIKit
+
+class AlbumView: UIView
+{
+    private var coverImage: UIImageView!
+    private var indicator:  UIActivityIndicatorView!
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+        commonInit();
+    }
+    
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    func commonInit()
+    {
+        let borderSize  = CGFloat(10)
+        
+        backgroundColor = UIColor.blackColor()
+        coverImage      = UIImageView(frame: CGRectMake(borderSize / 2, borderSize / 2,
+                                                        frame.size.width - borderSize, frame.size.height - borderSize))
+        addSubview(coverImage)
+        
+        
+        indicator = UIActivityIndicatorView()
+        indicator.center = center
+        indicator.activityIndicatorViewStyle = .WhiteLarge
+        indicator.startAnimating()
+        addSubview(indicator)
+    }
+    
+    
+    // ===================================================
+    
+    func highlightAlbum (didHightlightView didHightlightView: Bool)
+    {
+        backgroundColor = (didHightlightView ? UIColor.whiteColor() : UIColor.blackColor())
+    }
+    
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
