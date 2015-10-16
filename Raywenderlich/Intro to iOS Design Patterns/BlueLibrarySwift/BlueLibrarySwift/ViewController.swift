799ad6d74915459f3f8600a8e3e65e7db90c0d73
Christopher Fonseka <chris.fonseka@googlemail.com>
2015-10-16 23:06:14 +0100

    Table View delegates added to the main view controller

--- Raywenderlich/Intro to iOS Design Patterns/BlueLibrarySwift/BlueLibrarySwift/ViewController.swift
+++ Raywenderlich/Intro to iOS Design Patterns/BlueLibrarySwift/BlueLibrarySwift/ViewController.swift
@@ -22,21 +22,88 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    typealias AlbumData = (titles: [String], values:[String])
 
 	@IBOutlet var dataTable: UITableView!
-	@IBOutlet var toolbar: UIToolbar!
+	@IBOutlet var toolbar:   UIToolbar!
+    
+    private   var allAlbums         = [Album]()
+    private   var currentAlbumData: AlbumData?
+    private   var currentAlbumIndex = 0
 	
+    
+    // ===================================================
+    
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationController?.navigationBar.translucent = false
+        currentAlbumIndex = 0
+        
+        allAlbums = LibraryAPI.sharedInstance.getAlbums()
+        
+        dataTable.delegate       = self
+        dataTable.dataSource     = self
+        dataTable.backgroundView = nil
+        view.addSubview(dataTable)
+        
+        self.showDataForAlbum(currentAlbumIndex)
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+    
+    
+    // ===================================================
+    
+    func showDataForAlbum(albumIndex: Int)
+    {
+        if (albumIndex < allAlbums.count && albumIndex > -1)
+        {
+            let album = allAlbums[albumIndex]
+            currentAlbumData = album.ae_tableRepresentation()
+        }
+        else
+        {
+            currentAlbumData = nil
+        }
+        
+        dataTable.reloadData()
+    }
+}
 
 
+extension ViewController: UITableViewDataSource
+{
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        if let albumData = currentAlbumData
+        {
+            return albumData.titles.count
+        }
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        if let albumData = currentAlbumData
+        {
+            cell.textLabel!.text       = albumData.titles[indexPath.row]
+            cell.detailTextLabel?.text = albumData.values[indexPath.row]
+        }
+        return cell
+    }
+}
+
+
+extension ViewController: UITableViewDelegate
+{
+    
 }
 
