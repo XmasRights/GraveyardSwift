dd2fe121d59708d4a4ec9483933ae8c0e106e79c
Christopher Fonseka <chris.fonseka@googlemail.com>
2015-10-16 22:27:28 +0100

    Using the LibraryAPI class to hide functionality that involves both the Persistency Manager, and the HTTPClient - the Fascade Design Pattern in action

--- Raywenderlich/Intro to iOS Design Patterns/BlueLibrarySwift/BlueLibrarySwift/LibraryAPI.swift
+++ Raywenderlich/Intro to iOS Design Patterns/BlueLibrarySwift/BlueLibrarySwift/LibraryAPI.swift
@@ -20,4 +20,42 @@ class LibraryAPI: NSObject
         return Singleton.instance
     }
     
+    private let persistencyManager: PersistencyManager
+    private let httpClient:         HTTPClient
+    private let isOnline:           Bool
+    
+    override init()
+    {
+        persistencyManager = PersistencyManager()
+        httpClient         = HTTPClient()
+        isOnline           = false
+        
+        super.init()
+    }
+    
+    // ===================================================
+    
+    func getAlbums() -> [Album]
+    {
+        return persistencyManager.getAlbums()
+    }
+    
+    func addAlbum(album: Album, index: Int)
+    {
+        persistencyManager.addAlbum(album, index: index)
+        if isOnline
+        {
+            httpClient.postRequest("api/addAlbum", body: album.description)
+        }
+    }
+    
+    func deleteAlbum(index: Int)
+    {
+        persistencyManager.deleteAlbumAtIndex(index)
+        if isOnline
+        {
+            httpClient.postRequest("api/deleteAlbum", body: "\(index)")
+        }
+    }
+    
 }
