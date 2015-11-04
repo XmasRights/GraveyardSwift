a1a33357eafefe41f9fd7ccff19b6e1b53350b3d
XmasRights <chris.fonseka@googlemail.com>
2015-11-04 22:25:44 +0000

    New project to try out some card game physics

---
+++ Raywenderlich/ard Game Mechanics in Sprite Kit with Swift/RWCCG/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  RWCCG
+//
+//  Created by Brian Broom on 7/22/14.
+//  Copyright (c) 2014 Brian Broom. All rights reserved.
+//
+
+import Foundation
---
+++ Raywenderlich/ard Game Mechanics in Sprite Kit with Swift/RWCCG/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  RWCCG
+//
+//  Created by Brian Broom on 7/1/14.
+//  Copyright (c) 2014 Brian Broom. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+
+    }
+}
