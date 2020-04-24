697de2f8e08347aae07f3f15c835d81d1a9c07db
Christopher Fonseka <chris.fonseka@googlemail.com>
2020-04-24 22:20:08 +0100

    Made a slightly fancier button

--- Hacking with Swift/Neumorphism/neumorphism/ContentView.swift
+++ Hacking with Swift/Neumorphism/neumorphism/ContentView.swift
@@ -12,6 +12,14 @@ extension Color {
     public static let offWhite = Color(red: 225 / 255, green: 225 / 255, blue: 235 / 255)
 }
 
+extension LinearGradient {
+    init(_ colors: Color...) {
+        self.init(gradient: Gradient(colors: colors),
+                  startPoint: .topLeading,
+                  endPoint: .bottomTrailing)
+    }
+}
+
 struct ContentView: View {
     @State var counter = 0
 
@@ -56,8 +64,23 @@ struct SimpleButtonStyle: ButtonStyle {
                 if configuration.isPressed {
                     Circle()
                         .fill(Color.offWhite)
-                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: -5, y: -5)
-                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: 10, y: 10)
+                        .overlay(
+                            Circle()
+                                .stroke(Color.gray, lineWidth: 4)
+                                .blur(radius: 4)
+                                .offset(x: 2, y: 2)
+                                .mask(
+                                    Circle()
+                                        .fill(LinearGradient(Color.black, Color.clear))
+                            )
+                    )
+                        .overlay(
+                            Circle()
+                                .stroke(Color.white, lineWidth: 8)
+                                .blur(radius: 4)
+                                .offset(x: -2, y: -2)
+                                .mask(Circle().fill(LinearGradient(Color.clear, Color.black)))
+                    )
                 } else {
                     Circle()
                         .fill(Color.offWhite)
