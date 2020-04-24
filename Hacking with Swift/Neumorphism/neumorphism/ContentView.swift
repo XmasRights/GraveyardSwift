ffd41265b57cdb0970fd4ab7d8841b7ca5148627
Christopher Fonseka <chris.fonseka@googlemail.com>
2020-04-24 22:11:44 +0100

    I made a button :-D

--- Hacking with Swift/Neumorphism/neumorphism/ContentView.swift
+++ Hacking with Swift/Neumorphism/neumorphism/ContentView.swift
@@ -13,15 +13,28 @@ extension Color {
 }
 
 struct ContentView: View {
+    @State var counter = 0
+
     var body: some View {
         ZStack {
             Color.offWhite
 
-            RoundedRectangle(cornerRadius: 25)
-                .fill(Color.offWhite)
-                .frame(width: 300, height: 300)
-                .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
-                .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+            VStack {
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.offWhite)
+                    .frame(width: 300, height: 300)
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                    .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+
+                Button(action: {
+                    defer { self.counter += 1 }
+                    print("Tapped \(self.counter)")
+                }) {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.gray)
+                }
+                .buttonStyle(SimpleButtonStyle())
+            }
         }
         .edgesIgnoringSafeArea(.all)
     }
@@ -30,5 +43,28 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SimpleButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+        .padding(30)
+        .contentShape(Circle())
+        .background(
+            Group {
+                if configuration.isPressed {
+                    Circle()
+                        .fill(Color.offWhite)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: -5, y: -5)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: 10, y: 10)
+                } else {
+                    Circle()
+                        .fill(Color.offWhite)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                }
+            }
+        )
     }
 }
